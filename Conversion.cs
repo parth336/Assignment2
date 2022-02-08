a1750ac311f1d42b652a80d4eb165a73df1b3733
parth336 <62145214+parth336@users.noreply.github.com>
2022-02-07 19:07:19 -0500

    commit 1 conversion class

---
+++ Conversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Assignment1
+{
+    public class Conversion
+    {
+        private double conversionValue;
+        public double ConversionValue { get => conversionValue; set => conversionValue = value; }
+        public Conversion()
+        {
+            this.ConversionValue = 1;
+        }
+
+        public Conversion(double conversionvalue) {
+            this.ConversionValue = conversionValue;
+        }
+        public double ConvertCelciusToFahrenheit() {
+            return ((this.conversionValue * 9) / 5) + 5;
+        }
+        public double ConvertCelciusToKelvin() {
+            return this.conversionValue + 273.15;
+        }
+        public double ConvertKelvinToFahrenheit() {
+            return (((this.conversionValue - 273.15) * 9) / 5) + 32;
+        }
+        public double ConvertKelvinToCelsius() {
+            return this.conversionValue - 273.15;
+        }
+        public double ConvertFahrenheitToKelvin() { }
+        public double ConvertFahrenheitToCelsius() { }
+    }
+}
